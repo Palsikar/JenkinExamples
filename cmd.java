6af155fe6cf213d3717d64863d384c36ef855186
Palsikar <162675732+Palsikar@users.noreply.github.com>
2025-03-29 12:39:47 +0530

    Add files via upload

---
+++ cmd.java
@@ -0,0 +1,15 @@
+class cmd{
+public static void main(String args[]){
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+int r1=n1+n2;
+int r2=n1-n2;
+int r3=n1*n2;
+int r4=n1/n2;
+System.out.println("sum"+r1);
+System.out.println("diff"+r2);
+System.out.println("mul"+r3);
+System.out.println("div"+r4);
+}
+}
+
